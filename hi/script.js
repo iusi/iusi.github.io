98e791c7cc35412ebbaec5cf53b0c2ab32f672a7
iusi <72381647+iusi@users.noreply.github.com>
2023-05-01 14:24:27 +0800

    Delete script.js

--- hi/script.js
+++
@@ -1,9 +0,0 @@
-function copy() {
-    navigator.clipboard.writeText("hi@urmymoon.com");
-  }
-document.onkeyup = function(event) {
-    var keycode = event.key;
-    if (keycode === 'Enter') {
-        navigator.clipboard.writeText("hi@urmymoon.com");
-    }
-}
